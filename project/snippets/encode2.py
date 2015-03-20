3448b1f3ee0518cb6ccba2eed339f2f50c4823e1
Jelte Fennema <github-tech@jeltef.nl>
2015-03-20 14:22:00 +0100

    Remove snippets

--- project/snippets/encode2.py
+++
@@ -1,9 +0,0 @@
-from msgpack import packb, unpackb
-from zlib import compress, decompress
-
-def encode(data):
-    return compress(packb(data))
-
-def decode(data):
-    return unpackb(decompress(data))
-
--- project/snippets/hello.py
+++
@@ -1,8 +0,0 @@
-#!env/bin/python
-
-def main():
-    print('Hello, world!')
-
-if __name__ == '__main__':
-    main()
-
